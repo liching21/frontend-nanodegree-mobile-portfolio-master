592437d14ea1e0a960c97fbfe0908bf2709cdf7e
liching21 <li_ching92@hotmail.com>
2015-06-14 18:32:53 +1000

    optimised img and minified html

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,40 +4,53 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     pagespeed: {
-   options: {
-      nokey: true,
-      //url: "http://7887571b.ngrok.io"
+     options: {
+        nokey: true,
+        //url: "http://7887571b.ngrok.io"
+      },
+      /**
+      prod: {
+          url: "http://44fc39db.ngrok.io",
+          locale: "en_GB",
+          strategy: "desktop",
+          threshold: 30
+      },*/
+      prod: {
+          url: "http://69be0d8d.ngrok.io",
+          locale: "en_GB",
+          strategy: "mobile",
+          threshold: 30
+      },
+      /**prod: {
+          url: "http://7887571b.ngrok.io/views/pizza.html",
+          locale: "en_GB",
+          strategy: "desktop",
+          threshold: 30
+      },
+      prod: {
+          url: "http://7887571b.ngrok.io/views/pizza.html",
+          locale: "en_GB",
+          strategy: "mobile",
+          threshold: 30
+      }*/
     },
-    /**
-    prod: {
-        url: "http://44fc39db.ngrok.io",
-        locale: "en_GB",
-        strategy: "desktop",
-        threshold: 30
-    },*/
-    prod: {
-        url: "http://44fc39db.ngrok.io",
-        locale: "en_GB",
-        strategy: "mobile",
-        threshold: 30
-    },
-    /**prod: {
-        url: "http://7887571b.ngrok.io/views/pizza.html",
-        locale: "en_GB",
-        strategy: "desktop",
-        threshold: 30
-    },
-    prod: {
-        url: "http://7887571b.ngrok.io/views/pizza.html",
-        locale: "en_GB",
-        strategy: "mobile",
-        threshold: 30
-    }*/
-  }
+
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'index.html': 'src/index.html'
+        }
+      }
+    }
 
   });
 
   grunt.loadNpmTasks('grunt-pagespeed');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  //grunt.registerTask('default', ['ngrok']);
+  grunt.registerTask('default', ['htmlmin']);
 };
